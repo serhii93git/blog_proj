28a0cd162e78e1996fe757d0543a4feda80d328d
serhii93git <s.g.demchenko93@gmail.com>
2024-06-23 14:52:59 +0200

    dell link to post detail in post list

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -8,8 +8,8 @@
     <h4>Розповіді про те, що і як зі мною відбувалось закордоном 
       з домішками сухих фактів, власних емоцій та мрікувань</h4>
     <div v-for="post in posts" :key="post.id">
-      <h2 class="link">
-        <router-link :to="`/post/${post.id}`">{{ post.title }}</router-link>
+      <h2>
+        {{ post.title }}
       </h2>   
          
       <div class="content">
