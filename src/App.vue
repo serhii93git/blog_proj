1d1dfbceb855b467c1e972f4f2d52d0b27acaab7
serhii93git <s.g.demchenko93@gmail.com>
2024-06-26 21:44:35 +0200

    change nav bar and PostList style

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
 
 <style scoped>
 #app {
-  font-family: Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -29,27 +29,44 @@ export default {
 nav {
   display: flex;
   justify-content: space-around;
-  background-color: #007bff;
-  padding: 15px 0;
+  padding: 20px;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 20px auto;
+  max-width: 800px;
 }
 
 nav a {
-  color: white;
+  color: #333;
   text-decoration: none;
   font-size: 18px;
   padding: 10px 20px;
-  transition: background-color 0.3s, color 0.3s;
+  background-color: #ffffff;
+  border-radius: 10px;
+  transition: transform 0.3s, background-color 0.3s, color 0.3s;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 nav a:hover {
-  background-color: #0056b3;
+  background-color: #007bff;
   color: #fff;
-  border-radius: 5px;
+  transform: translateY(-5px);
 }
 
 nav a.router-link-exact-active {
   background-color: #0056b3;
   color: #fff;
-  border-radius: 5px;
+}
+
+@media (max-width: 768px) {
+  nav {
+    flex-direction: column;
+  }
+  
+  nav a {
+    margin-bottom: 10px;
+    text-align: center;
+  }
 }
 </style>
